834cb5b16d9224eca2bad506b124d78cdc8e92ca
Emmanuel Ogwo <42464913+theogwo@users.noreply.github.com>
2025-09-23 14:18:16 +0100

    Update headtts-node.mjs

--- modules/headtts-node.mjs
+++ modules/headtts-node.mjs
@@ -25,6 +25,8 @@ import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log("VoicePAth = "+path.join(__dirname, '../voices'))
+
 /**
 * Check if a specific command line option/flag was set.
 *
