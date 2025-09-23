a7f4a801dd904abad9c4b6b5db88c20540aab82a
Emmanuel Ogwo <42464913+theogwo@users.noreply.github.com>
2025-09-23 10:50:50 +0100

    Update headtts-node.mjs

--- modules/headtts-node.mjs
+++ modules/headtts-node.mjs
@@ -44,9 +44,6 @@ let restId = 0;
 const workers = new Array(config.tts.threads);
 const statuses = new Array(config.tts.threads);
 const mapRest = new Map();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const workerPath = path.join(__dirname, 'worker-tts.mjs');
 
 for (let i = 0; i < config.tts.threads; i++) {
